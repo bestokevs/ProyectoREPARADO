1fefbb14de03e026043606865aa496c5e3eb98df
bestokevs <kevin.viasus@hotmail.com>
2023-10-10 02:31:53 -0500

    Commit

---
+++ spring5webapp-SecurityProjectJohnThompson-master/src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,45 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by jt on 12/24/19.
+ */
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthrors(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+
+    @GetMapping("/authors/new")
+    public String readdata(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors/list";
+    }
+
+    @GetMapping("/authors/new/{nombre}/{apellido}")
+    public String createAuthor(Model model, @PathVariable("nombre") String nombre, @PathVariable("apellido") String apellido) {
+        String name = StringUtils.isEmpty(nombre) ? "Andres" : nombre;
+        String lastname = StringUtils.isEmpty(apellido) ? "Miranda" : apellido;
+        model.addAttribute("author", new Author());
+        Author author = new Author(name,lastname);
+        authorRepository.save(author);
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors/list";
+    }
+}
